3c8bcb617c66b26d322659f76a4daa8191359c24
haripriya-0712 <haripriyasudhakar712@gmail.com>
2025-10-29 18:30:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Navbar active link highlight
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 120;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLinks.forEach((a) => {
+    a.classList.remove("active");
+    if (a.getAttribute("href") === `#${current}`) {
+      a.classList.add("active");
+    }
+  });
+});
